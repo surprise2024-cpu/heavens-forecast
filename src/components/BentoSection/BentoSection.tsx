8f62f1f8ff06f7e82365d7b9c06cd7e30d8833f0
surprise2024-cpu <surprise2024@gmail.com>
2026-08-05 11:40:33 +0200

    update

--- src/components/BentoSection/BentoSection.tsx
+++ src/components/BentoSection/BentoSection.tsx
@@ -24,16 +24,6 @@ interface HourlyPoint {
     temp: number;
 }
 
-//Data
-{/*const hourly: HourlyPoint[] = [
-    {time: '6:00 AM', condition: 'cloudy', temp: 25},
-    {time: '9:00 AM', condition: 'cloudy', temp: 28},
-    {time: '12:00 PM', condition: 'sunny', temp: 33},
-    {time: '3:00 PM', condition: 'sunny', temp: 34},
-    {time: '6:00 PM', condition: 'sunny', temp: 32},
-    {time: '9:00 PM', condition: 'cloudy', temp: 30},
-];*/}
-
 interface BentoSectionProps {
     currentWeather: CurrentWeatherResponse | null;
     forecast: ForecastResponse | null;
